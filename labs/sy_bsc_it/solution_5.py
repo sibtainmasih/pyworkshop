953714ef5f771ec84d3689d2304588b2d7e70c76
Sibtain <sibtain.masih@gmail.com>
2018-08-26 17:15:40 +0530

    Using isdisjoint method to determine disjoint sets

--- labs/sy_bsc_it/solution_5.py
+++ labs/sy_bsc_it/solution_5.py
@@ -3,3 +3,9 @@ def common_check(input_list_1, input_list_2):
 	Returns `True` if both the lists have at least 1 common element
 	"""
 	return len(set(input_list_1) & set(input_list_2)) > 0
+
+def common_check_2(input_list_1, input_list_2):
+	"""
+	Returns `True` if both the lists have at least 1 common element
+	"""
+	return not (set(input_list_1)).isdisjoint(set(input_list_2))
